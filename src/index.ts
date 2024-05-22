528d9aa0911d0f1bd75dc6913d9285adb8385ccd
Johnny Oshika <johnnyoshika@gmail.com>
2024-05-22 12:35:23 -0700

    Use Pezzo managed prompt

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,32 @@
 import 'dotenv/config';
 import express from 'express';
-import OpenAI from 'openai';
+import { Pezzo, PezzoOpenAI } from '@pezzo/client';
+
 const app = express();
 
-const openai = new OpenAI({
-  baseURL: 'https://proxy.pezzo.ai/openai/v1',
-  defaultHeaders: {
-    'X-Pezzo-Api-Key': process.env.PEZZO_API_KEY,
-    'X-Pezzo-Project-Id': process.env.PEZZO_PROJECT_ID,
-    'X-Pezzo-Environment': 'Production',
-  },
+const pezzo = new Pezzo({
+  apiKey: process.env.PEZZO_API_KEY,
+  projectId: process.env.PEZZO_PROJECT_ID,
+  environment: 'Production',
 });
 
+const openai = new PezzoOpenAI(pezzo);
+
 app.get('/completion', async (req, res) => {
-  const completion = await openai.chat.completions.create({
-    model: 'gpt-3.5-turbo',
-    temperature: 0,
-    messages: [
-      {
-        role: 'user',
-        content: 'Tell me 5 fun facts about yourself',
-      },
-    ],
+  const numFacts = req.query.numFacts?.toString() ?? '3';
+  const topic = req.query.topic?.toString() ?? 'Cats';
+
+  const prompt = await pezzo.getPrompt('FactGenerator');
+
+  const completion = await openai.chat.completions.create(prompt, {
+    variables: {
+      numFacts,
+      topic,
+    },
+    properties: {
+      // You can optionally specify custom properties that will be associated with the request.
+      source: 'pezzo-ai app',
+    },
   });
 
   res.send(completion.choices[0].message.content);
