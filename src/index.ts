93b65fe3e55101fbe83bc0352b1c172b9a41b141
Johnny Oshika <johnnyoshika@gmail.com>
2024-05-22 11:40:05 -0700

    Add observability example

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,30 @@
 import 'dotenv/config';
 import express from 'express';
+import OpenAI from 'openai';
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send(
-    `Hi, process.env.SOME_ENV_VARIABLE is ${process.env.SOME_ENV_VARIABLE}`,
-  );
+const openai = new OpenAI({
+  baseURL: 'https://proxy.pezzo.ai/openai/v1',
+  defaultHeaders: {
+    'X-Pezzo-Api-Key': process.env.PEZZO_API_KEY,
+    'X-Pezzo-Project-Id': process.env.PEZZO_PROJECT_ID,
+    'X-Pezzo-Environment': 'Production',
+  },
+});
+
+app.get('/completion', async (req, res) => {
+  const completion = await openai.chat.completions.create({
+    model: 'gpt-3.5-turbo',
+    temperature: 0,
+    messages: [
+      {
+        role: 'user',
+        content: 'Tell me 5 fun facts about yourself',
+      },
+    ],
+  });
+
+  res.send(completion.choices[0].message.content);
 });
 
 const PORT = process.env.PORT || 8000;
